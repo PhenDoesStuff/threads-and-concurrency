2a8eb8134ea4464d238bdf965d074c9c07302b0c
Stephen Montague <stephen.montague@deposco.com>
2022-11-02 16:44:41 -0400

    Created another thread by creating a class and extending the Thread class. Start it by using the start() method.

--- src/com/practice/Main.java
+++ src/com/practice/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from the main thread.");
 
-        
+        // Initialize a new thread using the class that I created.
+        Thread anotherThread = new AnotherThread();
+
+        // Use the start() method to run the other thread.
+        anotherThread.start();
     }
 }
