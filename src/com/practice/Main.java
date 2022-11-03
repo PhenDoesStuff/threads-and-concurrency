243552061fb8ab532971e8eccec071a5dd99ccbe
Stephen Montague <stephen.montague@deposco.com>
2022-11-03 10:17:50 -0400

    Learning how to interrupt a thread.

--- src/com/practice/Main.java
+++ src/com/practice/Main.java
@@ -30,6 +30,7 @@ public class Main {
         });
 
         myRunnableThread.start();
+        anotherThread.interrupt();
 
         System.out.println(ThreadColor.ANSI_PURPLE + "Hello again from the main thread.");
 
